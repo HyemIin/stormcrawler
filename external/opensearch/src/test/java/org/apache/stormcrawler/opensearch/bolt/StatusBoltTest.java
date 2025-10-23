7bd5466a7d2f8d4d1ebaadc67501ac118f70e07f
jeonghyemin <hyem5019@gmail.com>
2025-10-23 15:44:39 +0900

    #1698 Revert import statement changes

--- external/opensearch/src/test/java/org/apache/stormcrawler/opensearch/bolt/StatusBoltTest.java
+++ external/opensearch/src/test/java/org/apache/stormcrawler/opensearch/bolt/StatusBoltTest.java
@@ -16,7 +16,7 @@
  */
 package org.apache.stormcrawler.opensearch.bolt;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
